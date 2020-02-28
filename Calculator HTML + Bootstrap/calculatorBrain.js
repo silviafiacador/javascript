a0d9efb5c9ddbde4ffd45153a6415d22442b49fb
= <=>
2020-02-28 16:49:19 -0300

    javascript 28-02-20

--- Calculator HTML + Bootstrap/calculatorBrain.js
+++ Calculator HTML + Bootstrap/calculatorBrain.js
@@ -1,46 +1,80 @@
+//variáveis GLOBAIS
 var result = document.getElementById("result");
-var n1=0, n2=0
+
+
+var n1, n2  //declaração de variáveis
 var operacao
+
+
 function onNumberClicked(numberButton){
   
-
-    if(result.innerHTML == "0"){
+    if (result.innerHTML == "0"){
         result.innerHTML = numberButton.innerHTML;
         }
     else{
         result.innerHTML += numberButton.innerHTML;
+        // result.innerHTML= result.innerHTML+ numberButton.innerHTML
        }
 }
 
 function onClearClicked(){
+    
     result.innerHTML = "0";
+    //"limpando" = zerando as variáveis de memória
+    n1=0;
+    n2=0;
 }
 
 function onEqualsClicked(){
-    n2= result.innerHTML
-    if (operacao =="+") 
-    {
-        result.innerHTML = parseInt(n1)+parseInt(n2)
-    }
+   n2=parseInt(result.innerHTML);   //atribuição e conversão para número
+   if (operacao=="+") {
+       result.innerHTML= n1+n2;
+      
+   }
+   if (operacao=="-") {
+       result.innerHTML= n1-n2;
+   }
+   if (operacao=="*") {
+       result.innerHTML= n1*n2;
+   }
+   if (operacao=="/") {
+       if (n2>0) {
+           result.innerHTML = n1/n2;}
+       else 
+          {result.innerHTML="Impossível a divisão por ZERO!!!!"}
+   }
+   
+
 }
 
 function onDivideClicked(){
-    operacao ="/"
+    n1=result.innerHTML
+    n1=parseInt(n1);  //retirando as aspas do n1
+    operacao ="/";
+    result.innerHTML="0";
 }
 
 
 
 function onAddClicked(){
-    n1= result.innerHTML
-    result.innerHTML = 0
-    operacao ="+"
+    n1=result.innerHTML
+    n1=parseInt(n1);  //retirando as aspas do n1
+    operacao ="+";
+    result.innerHTML="0";
+    
 }
 
 function onSubtractClicked(){
-    operacao ="-"
+    n1=result.innerHTML
+    n1=parseInt(n1);  //retirando as aspas do n1
+    operacao ="-";
+    result.innerHTML="0";
 }
 
 function onMultiplyCliked(){
-    operacao = "*"
+    n1=result.innerHTML
+    n1=parseInt(n1);  //retirando as aspas do n1
+    operacao ="*";
+    result.innerHTML="0";
 }
 
